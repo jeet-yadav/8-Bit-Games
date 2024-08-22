1c40634f87617749e6b3acc3359a5ec4a5cd101b
jeet-yadav <jeetyadav76468976@gmail.com>
2024-08-22 23:33:54 +0530

    added background image and added a lot better animation to navbar

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
-let star = document.querySelector(".p1 i");
-gsap.from(star,{
+function starRotate(){
+    let star = document.querySelector(".p1 i");
+    gsap.from(star,{
     rotate:360,
     duration: 2,
     repeat: -1,
     ease: "none"
-});
-star.addEventListener("hover", () => {
-    gsap.from(star,{
-        rotate:360,
-        duration: 0.5,
-        repeat: -1,
-        ease: "none"
     });
-})
+}
+let input = document.querySelector(".p3 input");
+let navLayer = document.querySelector(".navLayer");
+let p2 = document.querySelectorAll(".p2 h3");
+let menu = document.querySelector("#menu");
+let zoom = document.querySelector(".p3 img");
+function navAnimation(){
+    gsap.from(navLayer,{
+        duration: 0.5,
+        ease: "none",
+        opacity: 0,
+        delay:1
+    });
+    gsap.from(p2,{
+        y:10,
+        duration: 0.4,
+        ease: 'ease',
+        opacity: 0,
+        delay:1,
+        stagger:0.2
+    });
+    gsap.from(menu,{
+        x:20,
+        duration: 1,
+        ease: 'ease',
+        opacity: 0,
+        delay:1 
+    });
+    gsap.from(zoom,{
+        scale:0,
+        rotate:90,
+        duration: 1,
+        ease: 'ease',
+        opacity: 0,
+        delay:1
+    });
+    gsap.from(input,{
+        width :0,
+        duration: 2,
+        ease: 'power3.out',
+        opacity: 0,
+        delay:1
+    });
+}
+starRotate()
+navAnimation()
